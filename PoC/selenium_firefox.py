7b8bd1d50187144af4b6c6850e0dfd7134a8f09f
hannesroegiers <hannes.roegiers@student.hogent.be>
2024-04-29 20:27:35 +0200

    json formatted

--- PoC/selenium_firefox.py
+++ PoC/selenium_firefox.py
@@ -6,12 +6,8 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 def web_scrape(url):
     options = Options()
     options.add_argument("--enable-performance-logging")
-    # options.set_preference("profiling.enable_additional_profiling_types", True)
-    # options.set_preference("profiling.logging_prefs.performance", "ALL")
     options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
 
-    #service = Service(executable_path='C:\geckodriver.exe')
-    # driver = webdriver.Chrome(options=options)
     driver = webdriver.Chrome(options=options)
     driver.get(url)
     log = driver.get_log("performance")
@@ -19,10 +15,13 @@ def web_scrape(url):
     
     return log
     
-# url = "https://www.oddsportal.com/football/belgium/jupiler-pro-league/"
+
 url = "https://www.javatpoint.com/"
 scraped = web_scrape(url)
-# print(f"scraped title: {scraped}")
-# print(f"scraped log: {scraped}")
+with open("network_dump.txt", 'w') as f:
+    for i in scraped:
+        print(f"\n{i}")
+        f.write(i)
+    f.close()
 print(scraped)
 
